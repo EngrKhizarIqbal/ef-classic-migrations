d6d778966db50758f0eae55522d3111fc822c63a
KhizarIqbalEngr <KhizarIqbalEngr@gmail.com>
2019-11-21 10:03:28 +0500

    EF6 test with migration.

--- EF6WithCore.EF6/EF6WithCoreEF6Context.cs
+++ EF6WithCore.EF6/EF6WithCoreEF6Context.cs
@@ -1,24 +1,24 @@
 ﻿using Entities;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Data.Entity.ModelConfiguration.Conventions;
 
-namespace EF6Context
+namespace EF6WithCore.EF6
 {
-    public class EFContextForEF6 : DbContext
+    public class EF6WithCoreEF6Context : DbContext
     {
-        public EFContextForEF6() : base("DefaultConnection")
+        public EF6WithCoreEF6Context(string connString) : base(connString)
         {
             Configuration.AutoDetectChangesEnabled = false;
             Configuration.ProxyCreationEnabled = false;
             Configuration.LazyLoadingEnabled = false;
 
-            Database.SetInitializer(new NullDatabaseInitializer<EFContextForEF6>());
+            Database.SetInitializer(new NullDatabaseInitializer<EF6WithCoreEF6Context>());
         }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Configurations.Add(new CustomerConfiguration());
@@ -26,7 +26,14 @@ namespace EF6Context
         }
 
         public DbSet<Customer> Customers { get; set; }
-
         public DbSet<WebUser> WebUsers { get; set; }
+    }
+
+    public class EF6WithCoreEF6ContextFactory : IDbContextFactory<EF6WithCoreEF6Context>
+    {
+        public EF6WithCoreEF6Context Create()
+        {
+            return new EF6WithCoreEF6Context("Server=(localdb)\\mssqllocaldb; Database=EF6MVCCore; Trusted_Connection=True; MultipleActiveResultSets=true");
+        }
     }
 }
