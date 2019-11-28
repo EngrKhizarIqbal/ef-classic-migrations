50453498d1cab4f9779a9f714b058988cc69fa72
KhizarIqbalEngr <KhizarIqbalEngr@gmail.com>
2019-11-28 17:47:14 +0500

    Removed un-used project references and uncomment entities configutions in EFMigrations project.

--- EFMigrations/CustomerConfiguration.cs
+++ EFMigrations/CustomerConfiguration.cs
@@ -1,15 +1,15 @@
-﻿using System.Data.Entity.ModelConfiguration;
-using Entities;
+﻿using Entities;
+using System.Data.Entity.ModelConfiguration;
 
 namespace EFMigrations
 {
-    //internal class CustomerConfiguration : EntityTypeConfiguration<Customer>
-    //{
-    //    public CustomerConfiguration()
-    //    {
-    //        HasKey(key => key.Id);
+    internal class CustomerConfiguration : EntityTypeConfiguration<Customer>
+    {
+        public CustomerConfiguration()
+        {
+            HasKey(key => key.Id);
 
-    //        HasMany(m => m.WebUsers).WithMany(m => m.Customers);
-    //    }
-    //}
+            HasMany(m => m.WebUsers).WithMany(m => m.Customers);
+        }
+    }
 }
--- EFMigrations/WebUserConfiguration.cs
+++ EFMigrations/WebUserConfiguration.cs
@@ -1,15 +1,15 @@
-﻿//using System.Data.Entity.ModelConfiguration;
-//using Entities;
+﻿using System.Data.Entity.ModelConfiguration;
+using Entities;
 
-//namespace EFMigrations
-//{
-//    internal class WebUserConfiguration : EntityTypeConfiguration<WebUser>
-//    {
-//        public WebUserConfiguration()
-//        {
-//            HasKey(key => key.Id);
+namespace EFMigrations
+{
+    internal class WebUserConfiguration : EntityTypeConfiguration<WebUser>
+    {
+        public WebUserConfiguration()
+        {
+            HasKey(key => key.Id);
 
-//            HasMany(m => m.Customers).WithMany(m => m.WebUsers);
-//        }
-//    }
-//}
+            HasMany(m => m.Customers).WithMany(m => m.WebUsers);
+        }
+    }
+}
